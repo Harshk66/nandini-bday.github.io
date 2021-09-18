d76502828bbdea33412f913f5d81b89782a1b8ff
Harshk66 <60876769+Harshk66@users.noreply.github.com>
2021-09-18 21:54:32 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,75 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+numb: 1,
+    question: "Which is the most fun engg year?",
+    answer: "2nd",
+    options: [
+      "1st",
+      "2nd",
+      "3rd",
+      "4th"
+    ]
+  
+  },
+    {
+    numb: 2,
+    question: "Eating jain food _________",
+    answer: "forces you to secretly eat maggi at midnight",
+    options: [
+      "improves digestion",
+      "reduces pimples",
+      "helps you sleep better",
+      "forces you to secretly eat maggi at midnight"
+      
+    ]
+  },
+    {
+     numb: 3,
+    question: "What are you supposed to do when someone is asleep?",
+    answer: "Unka makeup kardo",
+    options: [
+      "Let them sleep",
+      "Paani daalke uthao",
+      "Unka makeup kardo",
+      "Unke baal bigaad do"
+    ]
+  },
+    {
+    numb: 4,
+    question: "There exists a one to one relation from a set X :={x|x is a song} to a set Y :={y|y is a vehicle}, if x1='Bachpan ka pyaar' y1=?",
+    answer: "Roadroller",
+    options: [
+      "Mercedes",
+      "Roadroller",
+      "Harley Davidson",
+      "Luna"
+    ]
+  },
+    {
+   
+  numb: 5,
+    question: "What is history, as of today?",
+    answer: "My teenage",
+    options: [
+      "Stoneage",
+      "Ironage",
+      "Digitalage",
+      "My teenage"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
